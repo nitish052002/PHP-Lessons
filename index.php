c5405aa8ff5bb27924c960fd49d0c677234a2e4e
Nitish <nitishsharma052002@gmail.com>
2024-06-05 00:31:28 +0530

    php lesson 1

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+        .text {
+            border: 1px solid #cacaca;
+            font-size: 1.3rem;
+            font-weight: 600;
+            text-align: center;
+            text-wrap: nowrap;
+            width: 350px;
+            padding: 1rem 2rem;
+            margin: 1rem;
+            text-transform: uppercase;
+            user-select: none;
+            position: relative;
+            transform: scale(1);
+            transition: 0.2s all ;
+            border-radius: 7px;
+
+         }
+
+        .text:hover {
+            transform: scale(1, 1.2);
+            border: 1px solid green;
+        }
+    </style>
+</head>
+
+<body>
+    <?php
+
+    $arr = array("ram", "shyam", "Gopi");
+
+    for ($i = 0; $i < count($arr); $i++) {
+        echo "<div class='text'> $arr[$i] </div>";
+    }
+
+
+
+    ?>
+</body>
+
+</html>
