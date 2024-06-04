01c6fce7f06af40fa13fbdbb18fc8265ec71230d
Nitish Sharma <131866146+nitish052002@users.noreply.github.com>
2024-06-05 00:58:10 +0530

    github linke inserted

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<!--     https://github.com/Dahkenangnon/PhpFromZero/blob/master/src/controllers/AuthController.php -->
 
     <style>
         .text {
